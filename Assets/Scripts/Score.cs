145ced9ac5c817738f2c04655d558d030ef5f500
Alfonsosh02 <alfonsohenao08@gmail.com>
2024-01-23 20:21:17 +0100

    Score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    private int score;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if(collision.gameObject.GetComponent<Moneda>())
+        {
+            score++;
+            scoreText.text = "COINS " + score;
+        }
     }
 }
